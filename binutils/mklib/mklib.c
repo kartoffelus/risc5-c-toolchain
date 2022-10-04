f6cb1adb23b8e56b2ac9e4d3fd7109415ca362a3
Hellwig Geisse <hellwig.geisse@mni.thm.de>
2022-10-04 16:49:15 +0200

    correct endianness

--- binutils/mklib/mklib.c
+++ binutils/mklib/mklib.c
@@ -58,18 +58,18 @@ void memFree(void *p) {
 
 
 unsigned int read4FromEco(unsigned char *p) {
-  return (unsigned int) p[0] << 24 |
-         (unsigned int) p[1] << 16 |
-         (unsigned int) p[2] <<  8 |
-         (unsigned int) p[3] <<  0;
+  return (unsigned int) p[0] <<  0 |
+         (unsigned int) p[1] <<  8 |
+         (unsigned int) p[2] << 16 |
+         (unsigned int) p[3] << 24;
 }
 
 
 void write4ToEco(unsigned char *p, unsigned int data) {
-  p[0] = data >> 24;
-  p[1] = data >> 16;
-  p[2] = data >>  8;
-  p[3] = data >>  0;
+  p[0] = data >>  0;
+  p[1] = data >>  8;
+  p[2] = data >> 16;
+  p[3] = data >> 24;
 }
 
 
