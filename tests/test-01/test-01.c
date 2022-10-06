d850d8a76493233d42fb5d8eede4fe9af6ebfa27
Hellwig Geisse <hellwig.geisse@mni.thm.de>
2022-10-06 12:42:32 +0200

    expand test-01

--- tests/test-01/test-01.c
+++ tests/test-01/test-01.c
@@ -3,16 +3,16 @@
  */
 
 
-void f1(void) {
+void f01(void) {
 }
 
 
-int f2(void) {
+int f02(void) {
   return 42;
 }
 
 
-int f3(void) {
+int f03(void) {
   int x, y;
  
   x = 1;
@@ -20,38 +20,83 @@ int f3(void) {
   return x + y;
 }
 
-int f4(void) {
+int f04(void) {
   int x, y;
  
   x = 1;
-  f1();
+  f01();
   y = 2;
   return x + y;
 }
 
 
-int neg(int x) {
+int f05(int x) {
   return -x;
 }
 
 
-int f5(int x, int y) {
+int f06(int x, int y) {
   return x + y;
 }
 
 
-int f6(int x, int y) {
-  f1();
+int f07(int x, int y) {
+  return x - y;
+}
+
+
+int f08(int x, int y) {
+  return x * y;
+}
+
+
+int f09(int x, int y) {
+  return x / y;
+}
+
+
+int f10(int x, int y) {
+  return x % y;
+}
+
+
+unsigned int f11(unsigned int x, unsigned int y) {
   return x + y;
 }
 
 
-int f7(int x, int y) {
+unsigned int f12(unsigned int x, unsigned int y) {
+  return x - y;
+}
+
+
+unsigned int f13(unsigned int x, unsigned int y) {
+  return x * y;
+}
+
+
+unsigned int f14(unsigned int x, unsigned int y) {
+  return x / y;
+}
+
+
+unsigned int f15(unsigned int x, unsigned int y) {
+  return x % y;
+}
+
+
+int f16(int x, int y) {
+  f01();
+  return x + y;
+}
+
+
+int f17(int x, int y) {
   int a, b, c;
 
   a = x + y;
   b = x - y;
-  c = f5(a + b, a - b);
+  c = f06(a + b, a - b);
   return c;
 }
 
