3ef21f62e6fe135ea02c712f8257cc165bb22044
Jan Merle <jan.merle@mni.thm.de>
2022-12-07 21:11:57 +0100

    Add RISC5 specific header file to standard include path

---
+++ lib/include/risc5.h
@@ -0,0 +1,53 @@
+/*
+ * risc5.h -- RISC5 specific declarations and constants
+ */
+
+#ifndef __RISC5_H
+#define __RISC5_H
+
+#include <stdint.h>
+
+/*** Interrupts ***/
+
+/* interrupt vector table */
+extern uint32_t risc5_ivt[16];  /* defined in crt0.s */
+
+/* irq numbers**/
+#define RISC5_IRQ_HP_TIMER      (15U)
+#define RISC5_IRQ_MS_TIMER      (11U)
+#define RISC5_IRQ_RS232_0_RX    (7U)
+#define RISC5_IRQ_RS232_0_TX    (6U)
+#define RISC5_IRQ_BUTTONS       (3U)
+
+/*** Devices ***/
+
+/* Device base addresses */
+#define RISC5_IO_BASE		    (0xFFFFC0U)
+#define RISC5_XIO_BASE	        (0xFFFF80U)
+
+/* Standard device addresses */
+#define RISC5_IO_MS_TIMER	            ((volatile uint32_t*)((RISC5_IO_BASE) + (4*0)))
+#define RISC5_IO_SWITCH                 ((volatile uint32_t*)((RISC5_IO_BASE) + (4*1)))
+#define RISC5_IO_LED                    IO_SWITCH /* SWITCH: read only, LED: write only */
+#define RISC5_IO_SERIAL_DATA            ((volatile uint32_t*)((RISC5_IO_BASE) + (4*2)))
+#define RISC5_IO_SERIAL_STAT_CTRL       ((volatile uint32_t*)((RISC5_IO_BASE) + (4*3)))
+#define RISC5_IO_SPI_DATA               ((volatile uint32_t*)((RISC5_IO_BASE) + (4*4)))
+#define RISC5_IO_SPI_STAT_CTRL_CTRL     ((volatile uint32_t*)((RISC5_IO_BASE) + (4*5)))
+#define RISC5_IO_MOUSE_DATA_KBD_STAT    ((volatile uint32_t*)((RISC5_IO_BASE) + (4*6)))
+#define RISC5_IO_KBD_DATA               ((volatile uint32_t*)((RISC5_IO_BASE) + (4*7)))
+#define RISC5_IO_GPIO_DATA              ((volatile uint32_t*)((RISC5_IO_BASE) + (4*8)))
+#define RISC5_IO_GPIO_DIR               ((volatile uint32_t*)((RISC5_IO_BASE) + (4*9)))
+
+/* Extended device addresses */
+#define RISC5_IO_HP_TIMER_DATA          ((volatile uint32_t*)((RISC5_XIO_BASE) + (4*0)))
+#define RISC5_IO_HP_TIMER_CTRL          ((volatile uint32_t*)((RISC5_XIO_BASE) + (4*1)))
+#define RISC5_IO_LCD_DATA               ((volatile uint32_t*)((RISC5_XIO_BASE) + (4*2)))
+#define RISC5_IO_LCD_STAT_CTRL          ((volatile uint32_t*)((RISC5_XIO_BASE) + (4*3)))
+
+/*** Cpu Intrinsics defined in asm.def ***/
+
+extern void _asm_CLI(void);
+extern void _asm_STI(void);
+extern void _asm_NOP(void);
+
+#endif /* __RISC5_H */
