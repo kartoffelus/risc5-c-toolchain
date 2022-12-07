9bfdd61d4d985a55bf0fc6d050169c13f86e7cf1
Jan Merle <jan.merle@mni.thm.de>
2022-12-07 21:07:09 +0100

    Add minimal stdlib.h

---
+++ lib/include/stdlib.h
@@ -0,0 +1,15 @@
+/*
+ * stdlib.h -- standard library definitions
+ */
+
+#ifndef __STDLIB_H
+#define __STDLIB_H
+
+/*********************************************
+ * NOTE: This is a not a full implementation 
+ *       of stdlib.h, just the bare minimum
+ *********************************************/
+
+#include <stddef.h>
+
+#endif //__STDLIB_H
