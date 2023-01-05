8337987c41fe7db63bddc9deaea2aaccde2406b7
Jan Merle <jan.merle@mni.thm.de>
2023-01-05 15:54:01 +0100

    Fix syntax error

--- lib/libc/math/math.c
+++ lib/libc/math/math.c
@@ -5,7 +5,8 @@ double fabs(double x)
 {
     /* NOTE: double on RISC5 is 32bit */
     /* NOTE: the union is used to satisfy C aliasing rules */
-    union {double f; uint32_t i;} u = {x};
+    union {double f; uint32_t i;} u;
+    u.f = x;
     u.i &= 0x7FFFFFFF;  /* clear sign bit */
     return u.f;
 }
